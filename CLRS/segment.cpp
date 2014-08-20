0a3081d524f088c2a3051d2d1eb3e0f55926db7d
zhen wang <awang@marinsoftware.com>
2014-08-20 17:28:42 +0800

    added scanline test

--- CLRS/segment.cpp
+++ CLRS/segment.cpp
@@ -1,7 +1,10 @@
 #include<cstdio>
 #include<cstdlib>
+#include<iterator>
 #include<vector>
 #include<cmath>
+#include<set>
+#include<algorithm>
 
 using namespace std;
 
@@ -20,6 +23,14 @@ struct Vec2{
     //y1 y2
     return x*rhs.y - rhs.x*y;
   }
+  
+  bool operator==(const Vec2& rhs )const{
+    return x==rhs.x && y==rhs.y;
+  }
+  
+  static bool isLeftUp( const Vec2& l, const Vec2& r ){
+    return ( l.x <= r.x ) && ( l.y >= r.y );
+  }
 
 };
 
@@ -30,6 +41,12 @@ struct Segment{
   
   bool Intersect( const Segment& rhs )const{
     return intersection( *l,*r, *(rhs.l), *(rhs.r) );
+  }
+  //s1 above s2
+  static bool Above( const Segment* s1, const Segment* s2 ){
+    Vec2 baseline = *(s2->r)-*(s2->l);
+    Vec2 s1dir = *(s1->l)-*(s2->l);
+    return s1dir.cross(baseline) > 0 ;
   }
   
 private:
@@ -61,16 +78,64 @@ private:
     }
     return false;
   }
+  
 };
-
 struct EndPoint{
   EPType type;
   const Vec2* p;
   const Segment* s;
-  EndPoint( const Vec2* pt, const Segment* sg, EPType tp ):
-    p(pt),s(sg),type(tp){}
+  EndPoint( const Vec2* pt, const Segment* sg, EPType tp ): p(pt),s(sg),type(tp){}
   
+  static bool isLeftUp( const EndPoint* l, const EndPoint* r ){
+    return Vec2::isLeftUp( *(l->p), *(r->p) );
+  }
 };
+
+bool scanLineTestSegmentsIntersection( const vector<Segment>& sg ){
+  int n = sg.size();
+  vector<EndPoint*> endPoints(2*n);
+  for( int i=0; i<n; i++ ){
+    Segment s = sg[i];
+    //I was intended to use the stackobject here
+    //but the compiler wont let me
+    endPoints.push_back( new EndPoint( s.l, &s, EPType::Left ) );
+    endPoints.push_back( new EndPoint( s.r, &s, EPType::Right ) );
+  }
+  sort( endPoints.begin(), endPoints.end(), EndPoint::isLeftUp );
+
+  set<const Segment*, bool(*)(const Segment*, const Segment*) > segmentTree(Segment::Above);
+
+  for( int i=0; i<2*n; i++ ){
+    EndPoint ep = *(endPoints[i]);
+    if( ep.type == EPType::Left ){
+      auto ret = segmentTree.insert( (ep.s) );
+      if( ret.second == false ){
+        printf("sth wrong\n");
+      }
+      auto itr = ret.first;
+      auto pitr = prev(itr);
+      if( pitr != segmentTree.end() ){
+        if( (**pitr).Intersect(**itr) ) return true;
+      }
+      auto nitr = next(itr);
+      if( nitr != segmentTree.end() ){
+        if( (**nitr).Intersect(**itr) ) return true;
+      }
+    }
+    else if ( ep.type == EPType::Right ){
+      auto itr = segmentTree.find( ep.s );
+      auto pitr = prev(itr);
+      auto nitr = next(itr);
+      if( pitr != segmentTree.end() && nitr != segmentTree.end() ){
+        if( (**pitr).Intersect(**nitr) ) return true;
+        segmentTree.erase(itr);
+      }
+    }
+  }
+  
+  
+  return false;
+}
 
 void TestSegmentCases( const Segment& s1, const Segment& s2, bool expected ){
   static int cases = 0 ;
@@ -83,12 +148,41 @@ void TestSegmentCases( const Segment& s1, const Segment& s2, bool expected ){
   }
   cases++;
 }
+
+void TestSegmentCollectionCases( const vector<Segment> segments , bool expected ){
+  static int cases = 0 ;
+  bool actual = scanLineTestSegmentsIntersection(segments );
+  if(  actual == expected ){
+    printf( "CASE:%d PASSED\n", cases );
+  }
+  else{
+    printf( "CASE:%d FAILED, Expected:%d, But:%d\n", cases, expected, actual );
+  }
+  cases++;
+}
+
 void TestSegment(){
   Vec2 p00=Vec2(0,0),p01=Vec2(0,1),p10=Vec2(1,0),p11=Vec2(1,1);
   Segment s0001=Segment(p00,p01), s1011=Segment(p10,p11), s0011=Segment(p00,p11), s1001=Segment(p10,p01);
   TestSegmentCases(s0001,s1011, false);
   TestSegmentCases(s0011,s1001, true);
   
+  vector<Segment> sample1;
+  sample1.push_back( s0001 );
+  sample1.push_back( s1011 );
+  TestSegmentCollectionCases( sample1, false );
+
+  vector<Segment> sample2;
+  sample2.push_back( s0011 );
+  sample2.push_back( s1001 );
+  TestSegmentCollectionCases( sample2, true );
+
+  vector<Segment> sample3;
+  sample3.push_back( s0001 );
+  sample3.push_back( s1011 );
+  sample3.push_back( s0011 );
+  sample3.push_back( s1001 );
+  TestSegmentCollectionCases( sample3, true );
 }
 
 
