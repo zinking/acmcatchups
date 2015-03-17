1aecd54456fb445cfeb3e97ca9dd6441cdf8c591
zinking <zinking3@gmail.com>
2015-03-17 15:51:23 +0800

    DP and record

--- leetcode/wordbreak2.py
+++ leetcode/wordbreak2.py
@@ -3,30 +3,31 @@ class Solution:
     # @param dict, a set of string
     # @return a list of strings
     def wordBreak(self, s, dict):
-	self.result = []
-	def dfs( s, l, dict):
-	    ss = set(s)
-	    ds = set( "".join(dict))
-
-	    for cs in ss:
-		if cs not in ds:
-		    return
-	    if s == "" :
-		self.result.append( " ".join(l)) 
-		return
-	    dd = filter( lambda x:x in s, dict)
-	    for w in dd:
-		nw = len(w)
-		if s[0:nw] == w:
-		    l.append(w)
-		    dfs( s[nw:], l, dd )
-		    l.pop()
-	dfs( s,[],dict)
-	return self.result
-
-
+	dd = filter( lambda x:x in s, dict)
+	ns = len(s)
+	jl = [ 0 for i in range(ns+1) ];jl[0]=1
+	rr = [ [] for i in range(ns+1)];
+	for i in range(1,ns+1):
+	    for j in range(0,i):
+		cw = s[j:i]
+		dd = filter( lambda x:x in cw, dict)
+		if jl[j]==1 and cw in dd:
+		    jl[i] = 1
+		    iw = dd.index(cw)
+		    prev = rr[j]
+		    if len(prev) == 0:
+			rr[i].append([cw])
+		    else:
+			for w in prev:
+			    w.append( cw )
+			    rr[i].append( w )
+	
+	return rr[-1]
+		
 s=Solution()
 
+print s.wordBreak("a",["a"])
 print s.wordBreak("catsanddog", ["cat", "cats", "and", "sand", "dog"])
+
 
 print s.wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab", ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"])	    
